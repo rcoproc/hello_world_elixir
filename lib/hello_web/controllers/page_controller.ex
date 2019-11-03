c209ef3bae08a4c40b66f35c8df3ec0f888954ac
Ricardo Oliveira <rcoproc@gmail.com>
2019-11-03 05:41:43 -0300

    First Commit

---
+++ lib/hello_web/controllers/page_controller.ex
@@ -0,0 +1,18 @@
+defmodule HelloWeb.PageController do
+  use HelloWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+  #def show(conn, _params) do
+  #  page = %{title: "foo"}
+
+  #  render(conn, "show.json", page: page)
+  #end
+
+  #def index(conn, _params) do
+  #  pages = [%{title: "foo"}, %{title: "bar"}]
+
+  #  render(conn, "index.json", pages: pages)
+  #end
+end
---
+++ lib/hello_web/views/layout_view.ex
@@ -0,0 +1,8 @@
+defmodule HelloWeb.LayoutView do
+  use HelloWeb, :view
+
+
+  def title do
+    "Awesome New Title from Phoenix!"
+  end
+end
